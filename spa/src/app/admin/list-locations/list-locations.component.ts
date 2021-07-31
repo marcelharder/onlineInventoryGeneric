0fc2ead2da023e0e94696a1cf758091023e5ff2e
Your Name <you@example.com>
2021-07-31 11:40:53 +0200

    test3

--- spa/src/app/admin/list-locations/list-locations.component.ts
+++ spa/src/app/admin/list-locations/list-locations.component.ts
@@ -16,7 +16,7 @@ export class ListLocationsComponent implements OnInit {
   selectedHospital: number;
   selectedVendor: number;
   listOfAvailableVendors: Array<DropItem> = [];
-  sd = 0; adl = 0;
+  sd = 0; adl = 0; 
   locationDetails: Location = {
     locationId: 0,
     naam: "",
@@ -60,10 +60,10 @@ export class ListLocationsComponent implements OnInit {
 
   showDetails() { if (this.sd === 1) return true; }
   showAddLocation() { if (this.adl === 1) return true; }
+ 
 
   hospitalChanged() {
     this.sd = 1;
-    this.alertify.message("Getting hospital details now ...");
     this.hos.getDetails(this.selectedHospital).subscribe((next) => {
       this.locationDetails = next;
       // get List of available vendors
@@ -85,8 +85,16 @@ export class ListLocationsComponent implements OnInit {
   }
 
   deleteLocation() { 
-    this.hos.removeLocation(this.selectedHospital).subscribe(
-      (next)=>{this.alertify.message("Deleting ...");});
+    let hospitalName = "";
+    this.hos.getDetails(this.selectedHospital).subscribe((next)=>{
+      hospitalName = next.naam;
+      this.alertify.confirm("Are you sure to delete " + hospitalName, ()=>{
+        this.hos.removeLocation(this.selectedHospital).subscribe(
+          (next)=>{this.alertify.message("Deleting ...");});
+      });
+    })
+   
+   
      }
   addLocation() { 
     this.adl = 1;
@@ -109,6 +117,17 @@ export class ListLocationsComponent implements OnInit {
     this.locationDetails.vendors.push(dropItem);
     this.listOfAvailableVendors.splice(index, 1);
   }
+
+  saveLocation(){
+    // save the details to the server
+    this.hos.saveDetails(this.locationDetails).subscribe((next)=>{
+      // push the new location record on the list of locations
+      this.ah.push(this.locationDetails);
+      this.alertify.message("Saving ...");
+    }, (error)=>{this.alertify.error(error);})
+
+    
+   }
 
   showDetailsVendor(Value: number) { }
 
